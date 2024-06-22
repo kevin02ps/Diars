10453b11a1f101df4ab8751bba25d969f5089731
kevin02ps <kevin.garcialeo2002@gmail.com>
2024-06-22 08:44:02 -0500

    Revert "."
    This reverts commit 899c71f4102a6386054a678c5ee9c665a5634365.

--- ProyectoDiars/obj/Release/net8.0-windows/ProyectoDiars.AssemblyInfo.cs
+++ ProyectoDiars/obj/Release/net8.0-windows/ProyectoDiars.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoDiars")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f78abd72a6b336ade7104fbfd526607b5228eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffe7cd93b65d017c6f0669ea69a90ba396f32e38")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoDiars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoDiars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
